10a0947c6c0e9c3cb323599ac7f8c8e511f89753
programmer <info@report.ms>
2022-02-20 02:14:22 +0300

    update install

--- server/install.sh
+++ server/install.sh
@@ -63,7 +63,18 @@ sudo yum install python36 -y
 sudo yum install epel-release -y
 sudo yum install p7zip p7zip-plugins -y
 
-sudo curl https://report-ms.github.io/LocalManipulator-linux.zip --output LocalManipulator.zip
-sudo 7za x -o/LocalManipulator LocalManipulator.zip
+sudo curl https://report-ms.github.io/server/assets.zip --output server-assets.zip
+sudo 7za x -o/app-server server-assets.zip
+cd app-server
+sudo python setup.py
+
+copyTo local-manipulator/local-manipulator.service /usr/lib/systemd/system
+
+sudo systemctl enable local-manipulator.service
 
 sudo systemctl start docker
+
+sudo systemctl start local-manipulator.service
+
+cd app
+docker compose up
