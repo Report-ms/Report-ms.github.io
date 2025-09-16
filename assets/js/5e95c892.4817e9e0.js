e74e828510c83bd4d7d1f7fd1b8285dd8ba93fc9
kirillSk <sk.kirillg@gmail.com>
2025-09-16 23:31:30 +0300

    Migrate static site assets and remove legacy files
    Added new static assets, images, fonts, and HTML files for the site, including error and sitemap pages. Removed legacy demo data, build-soft-pro resources, old CSS/JS, and language-specific index files to clean up and update the project structure.

---
+++ assets/js/5e95c892.4817e9e0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreport_ms=self.webpackChunkreport_ms||[]).push([[647],{7121:(e,r,s)=>{s.r(r),s.d(r,{default:()=>l});s(6540);var t=s(4164),u=s(7559),a=s(5500),c=s(2831),n=s(1656),p=s(4848);function l(e){return(0,p.jsx)(a.e3,{className:(0,t.A)(u.G.wrapper.docsPages),children:(0,p.jsx)(n.A,{children:(0,c.v)(e.route.routes)})})}}}]);
---
+++ assets/js/aba21aa0.73699971.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreport_ms=self.webpackChunkreport_ms||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
